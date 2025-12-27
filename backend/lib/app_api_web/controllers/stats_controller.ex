372f35e88d079c1066005d20194ba0768380e696
solaar45 <solaar45@yahoo.de>
2025-12-27 13:25:32 +0100

    misc

--- backend/lib/app_api_web/controllers/stats_controller.ex
+++ backend/lib/app_api_web/controllers/stats_controller.ex
@@ -660,45 +660,52 @@ defmodule AppApiWeb.StatsController do
 
   defp get_artist_cover_from_id3(artist_name) do
     # Hole den neuesten Listen-Eintrag für diesen Artist
-    listen = Repo.one(
-      from(l in Listen,
-        where: l.artist_name == ^artist_name,
-        where: not is_nil(l.metadata),
-        order_by: [desc: l.listened_at],
-        limit: 1
+    listen =
+      Repo.one(
+        from(l in Listen,
+          where: l.artist_name == ^artist_name,
+          where: not is_nil(l.metadata),
+          order_by: [desc: l.listened_at],
+          limit: 1
+        )
       )
-    )
 
     extract_cover_from_metadata(listen)
   end
 
   defp get_album_cover_from_id3(album_name, artist_name) do
     # Hole den neuesten Listen-Eintrag für Album + Artist
-    listen = Repo.one(
-      from(l in Listen,
-        where: l.release_name == ^album_name,
-        where: l.artist_name == ^artist_name,
-        where: not is_nil(l.metadata),
-        order_by: [desc: l.listened_at],
-        limit: 1
+    listen =
+      Repo.one(
+        from(l in Listen,
+          where: l.release_name == ^album_name,
+          where: l.artist_name == ^artist_name,
+          where: not is_nil(l.metadata),
+          order_by: [desc: l.listened_at],
+          limit: 1
+        )
       )
-    )
 
     extract_cover_from_metadata(listen)
   end
 
   defp extract_cover_from_metadata(nil), do: nil
+
   defp extract_cover_from_metadata(%{metadata: metadata}) when is_binary(metadata) do
     case Jason.decode(metadata) do
       {:ok, meta_map} -> extract_cover_url(meta_map)
       {:error, _} -> nil
     end
   end
+
   defp extract_cover_from_metadata(_), do: nil
 
   defp extract_cover_url(meta_map) when is_map(meta_map) do
-    # Verschiedene mögliche Keys für CoverArt in ID3 Tags
     cond do
+      # ✅ FIX: Subsonic cover_art URL (vom Navidrome-Scrobbling)
+      Map.has_key?(meta_map, "cover_art") ->
+        meta_map["cover_art"]
+
       # Navidrome: coverArt ID
       Map.has_key?(meta_map, "coverArt") ->
         build_navidrome_cover_url(meta_map["coverArt"])
@@ -719,11 +726,13 @@ defmodule AppApiWeb.StatsController do
         nil
     end
   end
+
   defp extract_cover_url(_), do: nil
 
   defp build_navidrome_cover_url(cover_art_id) when is_binary(cover_art_id) do
     navidrome_url = Application.get_env(:app_api, :navidrome_url, "http://localhost:4533")
     "#{navidrome_url}/rest/getCoverArt.view?id=#{cover_art_id}&size=200"
   end
+
   defp build_navidrome_cover_url(_), do: nil
 end
