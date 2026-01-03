b574f36af4f6845a6a720845b2afd8f7055c7da4
solaar45 <92338974+solaar45@users.noreply.github.com>
2026-01-03 12:06:37 +0100

    refactor(frontend): add main barrel exports for components

---
+++ frontend/src/components/features/index.ts
@@ -0,0 +1,5 @@
+// Feature Pages - Barrel Export
+export * from './overview'
+export * from './recent-listens'
+export * from './statistics'
+export * from './settings'
---
+++ frontend/src/components/index.ts
@@ -0,0 +1,15 @@
+// Layout Components
+export * from './layout'
+
+// Feature Pages
+export * from './features'
+
+// Shared Components
+export { default as DashboardContent } from './DashboardContent'
+export { StatsTable } from './StatsTable'
+export { StatsCover } from './StatsCover'
+export { DateTimeSettings } from './DateTimeSettings'
+export { TokenManager } from './TokenManager'
+export { NavidromeSetup } from './NavidromeSetup'
+export { DataExportImport } from './DataExportImport'
+export { MetadataEnrichment } from './MetadataEnrichment'
